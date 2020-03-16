78ef39e2dd846547d1cccf3898fd80eeb0c0dc7e
JakAdk <jake.adkin@arup.com>
2020-03-16 15:25:53 +0000

    #10 added response status codes

--- DeskCheck/DeskCheck/Controllers/UserController.cs
+++ DeskCheck/DeskCheck/Controllers/UserController.cs
@@ -24,22 +24,28 @@ namespace DeskCheck.Controllers
         }
 
         [HttpPost("sendNotif")]
-        public void SendNotif()
+        public IActionResult SendNotif()
         {
             string name = Request.Headers["Name"];
             string content = Request.Headers["Content"];
+            if(name == null || content == null)
+            {
+                return BadRequest();
+            }
             Execute(name, content).Wait();
+            return Ok();
         }
 
         static async Task Execute(string name, string content)
         {
+            var message = "User: " + name + " reported problem" + content;
             var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("Desk@check.com", name);
+            var from = new EmailAddress("Desk@check.com", "Desk Check");
             var subject = "Issue";
             var to = new EmailAddress("jake.adkin@arup.com", "Jake Adkin");
-            var plainTextContent = content;
-            var htmlContent = content;
+            var plainTextContent = message;
+            var htmlContent = message;
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
         }
